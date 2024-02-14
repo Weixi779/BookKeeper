87b52afdc67ee0a8a82413f3359985b297328cce
WeiXi <sunshiwei418@126.com>
2024-02-14 23:22:24 +0800

    First Version

---
+++ BookKeeper/BookKeeper/BookKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookKeeperApp.swift
+//  BookKeeper
+//
+//  Created by 孙世伟 on 2024/2/14.
+//
+
+import SwiftUI
+
+@main
+struct BookKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
